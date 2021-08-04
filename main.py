5167d76987a0e109a291b0e6d6b177483f742d9f
Blair Hoddinott <blair@lyricalsecurity.com>
2021-08-03 23:11:25 -0400

    BH: Switched to cv2, and implemented basic motion detectio and tracking

--- main.py
+++ main.py
@@ -1,38 +1,68 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
-# main.py
-# import the necessary packages
-from flask import Flask, render_template, Response, request
-from camera import VideoCamera
-import time
-import threading
-import os
+from flask import Flask, render_template, Response
+import cv2
+import datetime
+import imutils
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
-
-# App Globals (do not edit)
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+MIN_AREA = 2000
+
 
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
+    return render_template('index.html')
 
-def gen(camera):
-    #get camera frame
+
+def gen_frames():
+    firstFrame = None
     while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+            if firstFrame is None:
+                firstFrame = gray
+                continue
+
+            frameDelta = cv2.absdiff(firstFrame, gray)
+            threshold = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            threshold = cv2.dilate(threshold, None, iterations=2)
+            contours = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(contours)
+
+            for c in contours:
+                if cv2.contourArea(c) < MIN_AREA:
+                    continue
+            
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                text = "Da fuq"
+
+                cv2.putText(frame, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(
+                    frame,
+                    datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                    (10, frame.shape[0] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.35,
+                    (0, 0, 255),
+                    1
+                )
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', debug=False)
-    
-
-
